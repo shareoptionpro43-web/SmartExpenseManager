60f4cc979cc4857885776e55a62dafe51b511609
shareoptionpro43-web <shareoptionpro43@gmail.com>
2026-03-26 16:34:24 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -1,38 +1,18 @@
 package com.example.expensemanager
 
 import android.os.Bundle
-import android.widget.*
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val amount = findViewById<EditText>(R.id.etAmount)
-        val category = findViewById<EditText>(R.id.etCategory)
-        val date = findViewById<EditText>(R.id.etDate)
-        val notes = findViewById<EditText>(R.id.etNotes)
-        val saveBtn = findViewById<Button>(R.id.btnSave)
+        val tv = TextView(this)
+        tv.text = "App is Working ✅"
+        tv.textSize = 20f
 
-        saveBtn.setOnClickListener {
-
-            val amt = amount.text.toString()
-            val cat = category.text.toString()
-            val dt = date.text.toString()
-            val note = notes.text.toString()
-
-            if (amt.isEmpty() || cat.isEmpty()) {
-                Toast.makeText(this, "Please enter required fields", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Expense Saved!", Toast.LENGTH_SHORT).show()
-
-                amount.text.clear()
-                category.text.clear()
-                date.text.clear()
-                notes.text.clear()
-            }
-        }
+        setContentView(tv)
     }
 }
